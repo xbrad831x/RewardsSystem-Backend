17ab33e89a95abe7cd91c0f0dbc9e275c3270560
Brandon <mercadobrandon33@yahoo.com>
2018-06-17 23:18:14 -0700

    i think it works now

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-iron-east-04.cleardb.net'
 mysql.init_app(app)
 
 conn = mysql.connect()
-cursor = conn.cursor()
 
 @app.route("/number", methods=['POST'])
 def post_phonenumber():
+    cursor = conn.cursor()
     req = request.get_json()
     number = req['number'] 
     obj = hashlib.sha256(number.encode())
@@ -27,16 +27,21 @@ def post_phonenumber():
     cursor.execute("SELECT * FROM users WHERE number=\'" + val + "\'")
     row = cursor.fetchone()
     conn.commit()
+    cursor.close()
     if row is None:
         return json.dumps({"msg": "Please enter your name."})
     else:
         stamps = row[2] + 1
         name = row[0]
         if stamps == 11:
+            cursor=conn.cursor()
             cursor.execute("UPDATE users SET stamps=\'0\' WHERE number=\'" + val + "\'")
+            cursor.close()
             return json.dumps({"msg": "Welcome back! Please show this message to claim your free meal!"})
         else:
+            cursor=conn.cursor()
             cursor.execute("UPDATE users SET stamps=\'" + str(stamps) + "\' WHERE number=\'" + val + "\'")
+            cursor.close()
             return json.dumps({"name": name, "stamps": stamps})
 
 @app.route("/signup", methods=['POST'])
@@ -46,8 +51,10 @@ def signup():
     name = req['name']
     obj = hashlib.sha256(number.encode())
     val = obj.hexdigest()
+    cursor=conn.cursor()
     cursor.execute("INSERT INTO users (name, number, stamps) VALUES (\'" + name + "\', \'" + val +  "', '1')")
     conn.commit()
+    cursor.close()
     return json.dumps({"name": name, "stamps": 1})
 
 if __name__ == "__main__":
