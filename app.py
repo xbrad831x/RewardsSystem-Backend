26567dcf322aa03be4bf42cb6fd9f75a7c4909dc
Brandon <mercadobrandon33@yahoo.com>
2018-06-19 15:05:54 -0700

    fixed bug with row[3]

--- app.py
+++ app.py
@@ -26,16 +26,16 @@ def post_phonenumber():
     cursor.execute("SELECT * FROM users WHERE number=\'" + val + "\'")
     row = cursor.fetchone()
     now = datetime.datetime.now()
-    limit = datetime.timedelta(hours=1)
-    timeDifference = now - row[3]
-    isPastLimit = timeDifference < limit   
+    limit = datetime.timedelta(hours=1)   
     if row is None:
         cursor.close()
         return json.dumps({"msg": "Please enter your name."})
-    elif isPastLimit:
-        cursor.close()
-        return json.dumps({"msg": "I'm sorry, but you can only check in once a day."})
     else:
+         timeDifference = now - row[3]
+         isPastLimit = timeDifference < limit
+        if isPastLimit:
+            cursor.close()
+            return json.dumps({"msg": "I'm sorry, but you can only check in once a day."})
         stamps = int(row[2]) + 1
         name = row[0]
         if stamps == 11:
